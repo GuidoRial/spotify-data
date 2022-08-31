443db7733d12057466afb52ae83ae6e7ddf18aac
GuidoRial <rialguido@gmail.com>
2022-08-31 14:42:00 -0300

    Clean code

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 const express = require("express");
-const SpotifyWebApi = require("spotify-web-api-node");
 const app = express();
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const lyricsFinder = require("lyrics-finder");
 const setupRoutes = require("./server/api/routes");
 
 app.use(cors());
@@ -11,10 +9,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 setupRoutes(app);
-
-app.get("/lyrics", async (req, res) => {
-
-});
 
 app.listen("3001", () => {
   console.log("Server running on port 3001");

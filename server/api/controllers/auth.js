386ef22377f86c66e1e7948f1ce4ec8eff617d76
GuidoRial <rialguido@gmail.com>
2023-03-11 13:01:23 -0300

    Remove use of spotify-web-api-node at login

--- server/api/controllers/auth.js
+++ server/api/controllers/auth.js
@@ -1,22 +1,43 @@
-const SpotifyWebApi = require("spotify-web-api-node");
+const axios = require('axios');
 
 module.exports = {
   async login(req, res) {
     try {
-      const code = req.body.code;
-      const spotifyApi = new SpotifyWebApi({
-        redirectUri: "http://localhost:8080/login",
-        clientId: process.env.CLIENT_ID,
-        clientSecret: process.env.CLIENT_SECRET,
+      const url = `${process.env.BASE_URL}/api/token`;
+
+      const details = {
+        code: req.body.code,
+        redirect_uri: process.env.REDIRECT_URI,
+        grant_type: 'authorization_code',
+      };
+
+      const body = new URLSearchParams(details).toString();
+
+      const response = await axios({
+        method: 'POST',
+        url,
+        headers: {
+          Authorization: 'Basic ' + Buffer.from(process.env.CLIENT_ID + ':' + process.env.CLIENT_SECRET).toString('base64'),
+          'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
+        },
+        data: body,
       });
 
-      const data = await spotifyApi.authorizationCodeGrant(code);
+      const {
+        data: { access_token, refresh_token, expires_in },
+      } = response;
 
       return res.status(200).json({
-        accessToken: data.body.access_token,
-        refreshToken: data.body.refresh_token,
-        expiresIn: data.body.expires_in,
+        accessToken: access_token,
+        refreshToken: refresh_token,
+        expiresIn: expires_in,
       });
+    } catch (e) {
+      return res.status(400).json({ message: e.message, stack: e.stack });
+    }
+  },
+  async refreshAccessToken(req, res) {
+    try {
     } catch (e) {
       return res.status(400).json({ message: e.message, stack: e.stack });
     }
